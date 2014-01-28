5e45d085e8d4235c29f2c7a928c974161009dff5
Class_Account <cs189-cj@hive11.CS.Berkeley.EDU>
2014-01-27 21:21:50 -0800

    init

---
+++ code/ex1.m
@@ -0,0 +1,27 @@
+training_set = train{1};
+
+labels = [];
+trainingfeat = [];
+
+for i = 1: length(training_set)  
+    
+    % get all labels in our development training set
+    labels = [labels, (training_set(i).labels)'];
+    
+    %get all the training features in our dataset
+    for j = 1: length(training_set(i).images)
+        
+        % each image is a 28x28 array of pixels
+        pixels = training_set(i).images(:,:,j);
+        % for this naive approach, 
+        % we will turn it into a row vector with
+        % all the pixel values concatinated 
+        row = reshape(pixels,1,[]);
+        trainingfeat = [trainingfeat; row];
+        
+    end
+end
+
+lab = double(labels)';
+img = sparse(double(trainingfeat));
+model=train(lab, img, '-s 0')
